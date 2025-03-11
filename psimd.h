b0a4af06f51fb6d2d9faa1e0fd21495abbcf9768
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2025-03-11 16:04:24 -0700

    Make `run_on_basic_block` static

--- psimd.h
+++ psimd.h
@@ -7,7 +7,7 @@
 struct psimd : public llvm::PassInfoMixin<psimd> {
   llvm::PreservedAnalyses run(llvm::Function &function,
                               llvm::FunctionAnalysisManager &);
-  bool run_on_basic_block(llvm::BasicBlock &basic_block);
+  static bool run_on_basic_block(llvm::BasicBlock &basic_block);
 
   // Without isRequired returning true, this pass will be skipped for functions
   // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
