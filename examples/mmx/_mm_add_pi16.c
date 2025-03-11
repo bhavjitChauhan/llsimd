75efeb6a260a7db3da24b659d99c8c3b95758279
creamlaflare <aka184@sfu.ca>
2025-03-11 16:04:01 -0700

    Update `paddw` and `paddd` examples

--- examples/mmx/_mm_add_pi16.c
+++ examples/mmx/_mm_add_pi16.c
@@ -1,15 +1,12 @@
 #include <mmintrin.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 int main() {
-    __v4hi m1 = {0, 1, 2, 3};
-    __v4hi m2 = {8, 9, 10, 11};
-    __v4hi result = _mm_add_pi16(m1, m2);
+  __v4hi m1 = {0, 1, INT8_MAX, INT8_MIN};
+  __v4hi m2 = {4, 5, 1, -1};
+  __v4hi result = _mm_add_pi16(m1, m2);
 
-    for (uint8_t i = 0; i < 4; ++i)
-        printf("%d\n", result[i]);
-
-    _mm_empty();
-    return 0;
+  for (uint8_t i = 0; i < 4; ++i)
+    printf("%d\n", result[i]);
 }
