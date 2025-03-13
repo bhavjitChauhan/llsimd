603b959e347b6200e4577d48de06097003f9976b
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2025-03-12 21:47:25 -0700

    Implement `psrli.*`

--- psimd.cpp
+++ psimd.cpp
@@ -92,7 +92,6 @@ bool psimd::run_on_basic_block(BasicBlock &basic_block) {
         default:
           llvm_unreachable("Unknown intrinsic");
         }
-
         Value *undef = UndefValue::get(m->getType());
         Value *insert =
             builder.CreateInsertElement(undef, convert, builder.getInt32(0));
@@ -144,7 +143,6 @@ bool psimd::run_on_basic_block(BasicBlock &basic_block) {
         default:
           llvm_unreachable("Unknown intrinsic");
         }
-
         Value *undef = UndefValue::get(m1->getType());
         Value *insert =
             builder.CreateInsertElement(undef, convert, builder.getInt32(0));
@@ -169,6 +167,42 @@ bool psimd::run_on_basic_block(BasicBlock &basic_block) {
         Value *shuffle = builder.CreateShuffleVector(
             m2, undef, ConstantAggregateZero::get(m2->getType()));
         Value *result = builder.CreateLShr(m1, shuffle);
+
+        call_inst->replaceAllUsesWith(result);
+        to_remove.push_back(&instruction);
+      } else if (intrinsic_name.consume_front("psrli.")) {
+        /*
+        ; %result = call <8 x i16> @llvm.x86.sse2.psrli.w(<8 x i16> %m, i32 %count)
+        %trunc = trunc i32 %count to i16
+        %insert = insertelement <8 x i16> undef, i16 %trunc, i32 0
+        %shuffle = shufflevector <8 x i16> %insert, <8 x i16> undef, <8 x i32> zeroinitializer
+        %result = lshr <8 x i16> %m, %shuffle
+        */
+
+        Value *m = call_inst->getArgOperand(0);
+        Value *count = call_inst->getArgOperand(1);
+
+        IRBuilder<> builder(&instruction);
+        Value *convert = nullptr;
+        switch (m->getType()->getScalarSizeInBits()) {
+        case 16:
+          convert = builder.CreateTrunc(count, m->getType()->getScalarType());
+          break;
+        case 32:
+          convert = count;
+          break;
+        case 64:
+          convert = builder.CreateZExt(count, m->getType()->getScalarType());
+          break;
+        default:
+          llvm_unreachable("Unknown intrinsic");
+        }
+        Value *undef = UndefValue::get(m->getType());
+        Value *insert =
+            builder.CreateInsertElement(undef, convert, builder.getInt32(0));
+        Value *shuffle = builder.CreateShuffleVector(
+            insert, undef, ConstantAggregateZero::get(m->getType()));
+        Value *result = builder.CreateLShr(m, shuffle);
 
         call_inst->replaceAllUsesWith(result);
         to_remove.push_back(&instruction);
