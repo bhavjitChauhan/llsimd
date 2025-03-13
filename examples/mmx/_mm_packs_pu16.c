2743c80f51dd0bf9e20f35d299b26a73979fee1c
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2025-03-13 13:11:04 -0700

    Implement `packuswb.128`

--- examples/mmx/_mm_packs_pu16.c
+++ examples/mmx/_mm_packs_pu16.c
@@ -4,8 +4,8 @@
 
 int main() {
   __v4hi m1 = {0, 1, 2, 3};
-  __v4hi m2 = {INT8_MAX, INT8_MAX + 1, -1, INT8_MIN};
-  __v8qi result = _mm_packs_pu16(m1, m2);
+  __v4hi m2 = {INT8_MAX, INT8_MAX + 1, -1, UINT8_MAX + 1};
+  __v8qu result = _mm_packs_pu16(m1, m2);
 
   for (uint8_t i = 0; i < 8; ++i)
     printf("%d\n", result[i]);
