1a334bb3a17a6e36e107919805e3e900c6d9b562
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2025-03-12 22:14:58 -0700

    Implement `pmadd.wd`

--- psimd.cpp
+++ psimd.cpp
@@ -47,7 +47,39 @@ bool psimd::run_on_basic_block(BasicBlock &basic_block) {
 
       ++mmx_intrinsic_count;
     } else if (intrinsic_name.consume_front("sse2.")) {
-      if (intrinsic_name.consume_front("psll.")) {
+      if (intrinsic_name == "pmadd.wd") {
+        /*
+        ; %result = call <4 x i32> @llvm.x86.sse2.pmadd.wd(<8 x i16> %m1, <8 x i16> %m2)
+        %sext1 = sext <8 x i16> %m1 to <8 x i32>
+        %sext2 = sext <8 x i16> %m2 to <8 x i32>
+        %mul = mul <8 x i32> %sext1, %sext2
+        %even = shufflevector <8 x i32> %mul, <8 x i32> undef, <4 x i32> <i32 0, i32 2, i32 4, i32 6>
+        %odd = shufflevector <8 x i32> %mul, <8 x i32> undef, <4 x i32> <i32 1, i32 3, i32 5, i32 7>
+        %result = add <4 x i32> %even, %odd
+        */
+
+        Value *m1 = call_inst->getArgOperand(0);
+        Value *m2 = call_inst->getArgOperand(1);
+
+        IRBuilder<> builder(&instruction);
+        Value *sext1 = builder.CreateSExt(
+            m1, VectorType::get(builder.getInt32Ty(), 8, false));
+        Value *sext2 = builder.CreateSExt(
+            m2, VectorType::get(builder.getInt32Ty(), 8, false));
+        Value *mul = builder.CreateMul(sext1, sext2);
+        Value *even = builder.CreateShuffleVector(
+            mul, UndefValue::get(mul->getType()),
+            ConstantDataVector::get(builder.getContext(),
+                                    ArrayRef<uint8_t>({0, 2, 4, 6})));
+        Value *odd = builder.CreateShuffleVector(
+            mul, UndefValue::get(mul->getType()),
+            ConstantDataVector::get(builder.getContext(),
+                                    ArrayRef<uint8_t>({1, 3, 5, 7})));
+        Value *result = builder.CreateAdd(even, odd);
+
+        call_inst->replaceAllUsesWith(result);
+        to_remove.push_back(&instruction);
+      } else if (intrinsic_name.consume_front("psll.")) {
         /*
         ; %result = call <8 x i16> @llvm.x86.sse2.psll.w(<8 x i16> %m1, <8 x i16> %m2)
         %shuffle = shufflevector <8 x i16> %m2, <8 x i16> undef, <8 x i32> zeroinitializer
